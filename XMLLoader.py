ffc13cb52428aa1b1cda2b4bfbcf13395530f56e
jeremy.quek@frasershospitality.com <jeremy.quek@frasershospitality.com>
2022-12-13 02:54:43 +0000

    update to deal with date and decimal types, also to deal with empty datasets

--- XMLLoader.py
+++ XMLLoader.py
@@ -1,28 +1,31 @@
 # Databricks notebook source
 from pyspark.sql.functions import size,col,when
 from pyspark.sql import SparkSession
-from pyspark.sql.types import TimestampType,FloatType
+from pyspark.sql.types import TimestampType,FloatType,DateType
 spark = SparkSession.builder.getOrCreate()
 
 def xmlLoader(source):
-  df_labels = spark.read.format('xml').options(rowTag='FIELD').load('dbfs:/FileStore/testing/rev.xml')
+  df_labels = spark.read.format('xml').options(rowTag='FIELD').load(source)
 
   #df_labels = spark.read.format('xml').options(rowTag='FIELD').load('abfss://fhg@fplsinut01datalakedlsa01.dfs.core.windows.net/testing/new.xml')
   df_labels=df_labels.filter("_label is not null")
 
   df2=df_labels.select("_label","_type").collect()
-  df_data = spark.read.format('xml').options(rowTag='R').load('dbfs:/FileStore/testing/rev.xml')
+  df_data = spark.read.format('xml').options(rowTag='R').load(source)
+  if df_data.count()>0:
+    for i in range(df_labels.count()):
+      if df2[i]._type =='DATETIME':
+        df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(TimestampType()) )
+      if df2[i]._type =='DATE':
+        df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(DateType()) )
+      elif df2[i]._type =='NUMBER':
+        df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(FloatType()) )
+      elif df2[i]._type =='DECIMAL':
+        df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(FloatType()) )
+      else:
+        df_data=df_data.withColumn(df2[i]._label,df_data.D[i] )
+    df_data=df_data.drop("D")
 
-  for i in range(df_labels.count()):
-    if df2[i]._type =='DATETIME':
-      df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(TimestampType()) )
-    elif df2[i]._type =='VARCHAR':
-      df_data=df_data.withColumn(df2[i]._label,df_data.D[i] )
-    else:
-      df_data=df_data.withColumn(df2[i]._label,df_data.D[i].cast(FloatType()) )
-  df_data=df_data.drop("D")
   return df_data
 
-#dbfs:/FileStore/testing/big_file.xml
-df_data=xmlLoader('abfss://fhg@fplsinut01datalakedlsa01.dfs.core.windows.net/testing/new.xml')
-df_data.display()
+
